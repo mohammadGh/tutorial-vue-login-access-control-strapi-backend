da725d800dbc4c93c1670b7fdd3f82cbaa8e8715
mgh <m.ghoreshi@gmail.com>
2023-02-20 17:28:24 +0330

    fix vite proxy for cms-apis

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,16 +3,18 @@ import axios from "axios"
 const isAuthenticated=()=>false
 const getAccessToken=()=>"access-token-sample"
 
+
+
 // axios instance
 const axiosService = axios.create({
-    baseURL: "/backendapi/api/", //import.meta.env.VITE_API_BASEPATH, //configure base_url for all requests
+    baseURL: "/cms-backend-api/api/", //configure base_url for all requests
     timeout: 30000// Request timeout time
   })
 
 // an interceptor to automatically add access-token to the authenticated requests
 axiosService.interceptors.request.use(
     (config) => {
-      if (isAuthenticated) {
+      if (isAuthenticated()) {
         const token = getAccessToken();
         config.headers["Authorization"] = 'Bearer ' + token;  
         config.headers["x-access-token"] = token;
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,20 @@ export default defineConfig({
   plugins: [vue()],
   server:{
     proxy:{
-    '/backendapi': {
+    '/cms-backend-api': {
       target: 'http://localhost:1337/',
-      rewrite: (path) => path.replace(/^\/backendapi/, ''),
+      rewrite: (path) => path.replace(/^\/cms-backend-api/, ''),
+      configure: (proxy, _options) => {
+        proxy.on('error', (err, _req, _res) => {
+          console.log('proxy error', err);
+        });
+        proxy.on('proxyReq', (proxyReq, req, _res) => {
+          console.log('Sending Request to the Target:', req.method, req.url, req.path);
+        });
+        proxy.on('proxyRes', (proxyRes, req, _res) => {
+          console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+        });
+      },
     }
   },
 }

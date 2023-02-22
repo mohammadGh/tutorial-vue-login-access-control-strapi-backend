409812a81121e267025b0910d662413a593eb065
mgh <m.ghoreshi@gmail.com>
2023-02-22 19:27:24 +0330

    add path alias

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,33 @@
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// more info for configuring Vite is at: https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    proxy:{
-    '/cms-backend-api': {
-      target: 'http://localhost:1337/',
-      rewrite: (path) => path.replace(/^\/cms-backend-api/, ''),
-      configure: (proxy, _options) => {
-        proxy.on('error', (err, _req, _res) => {
-          console.log('proxy error', err);
-        });
-        proxy.on('proxyReq', (proxyReq, req, _res) => {
-          console.log('Sending Request to the Target:', req.method, req.url, req.path);
-        });
-        proxy.on('proxyRes', (proxyRes, req, _res) => {
-          console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-        });
-      },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-}
-  
+  server: {
+    proxy: {
+      '/cms-backend-api': {
+        target: 'http://localhost:1337/',
+        rewrite: (path) => path.replace(/^\/cms-backend-api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url, req.path);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    },
+  }
+
 })
